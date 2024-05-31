33e5851028d499559e68c0c2202ad5a6fc4ced8a
MarcoRomanoHub <marcoromano270@gmail.com>
2024-05-31 16:39:25 +0200

    [add] Milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
     <script>
     import axios from 'axios'
       export default {
-        
+        data(){
+          return{
+
+          }
+        },
+
+        methods:{
+          getApi(){
+            axios.get('http://127.0.0.1:8000/api/projects')
+              .then(result =>{
+                console.log(result.data);
+              })
+              .catch(error =>{
+                console.log(error.message);
+              })
+          }
+        },
+
+        mounted(){
+          this.getApi();
+        }
       }
     </script>
 
