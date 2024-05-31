216243b97849faf81e9294f67bdb8cbe46ee0bfb
MarcoRomanoHub <marcoromano270@gmail.com>
2024-05-31 16:30:00 +0200

    [add] Milestone 3

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+    <script>
+    import axios from 'axios'
+      export default {
+        
+      }
+    </script>
+
+<template>
+  <div>
+    prova
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
