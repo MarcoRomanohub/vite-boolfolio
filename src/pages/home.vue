7173ae734a7a4ed45a195508f21254e92d78aa03
MarcoRomanoHub <marcoromano270@gmail.com>
2024-06-03 17:09:57 +0200

    [add] Api

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,12 +1,37 @@
     <script>
+    import { store } from '@/data/store';
+    import axios from 'axios';
       export default {
-        name: 'Home'
+        name: 'Home',
+        data(){
+          return{
+            projects:[]
+          }
+        },
+        methods:{
+          getApi(){
+            axios.get(store.apiUrl)
+              .then(result => {
+                this.projects=result.data
+              })
+              .catch( error => {
+
+              })
+          }
+        },
+        mounted(){
+          this.getApi();
+        }
+
       }
     </script>
 
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>I miei Post</h1>
+    <ul>
+      <li v-for="projects in projects" :key="projects.id">{{ projects.id }} - {{ projects.title }}</li>
+    </ul>
   </div>
 </template>
 
